cf3ad51d9a81516bf94d0df39fd0a9e52f493079
nosoul1988 <mario_no_soul@hotmail.com>
2014-09-22 11:17:49 -0300

    Add Rotation Loop in the class PBReactiveObjective

--- main/glbpaintball/Source/glbpaintball/PBReactiveObjective.h
+++ main/glbpaintball/Source/glbpaintball/PBReactiveObjective.h
@@ -3,15 +3,44 @@
 #pragma once
 
 #include "PBObjective.h"
+#include "GameFramework/Actor.h"
+#include "PBCharacter.h"
 #include "PBReactiveObjective.generated.h"
 
 /**
  * 
  */
+
+UENUM(BlueprintType) 
+enum EnumStatus
+{
+ ES_Vigilance UMETA(DisplayName = "Vigilance"),
+ ES_Attack UMETA(DisplayName = "Attack"),
+ ES_Dead UMETA(DisplayName = "Dead"),
+};
+
 UCLASS()
 class GLBPAINTBALL_API APBReactiveObjective : public APBObjective
 {
 	GENERATED_UCLASS_BODY()
+
+	/** Status of the Objective */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Status)
+	int32 Status;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ActorRotation)
+	float RotationSpeed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = ActorRotation)
+	float DegreesToRotate;
+
+	FRotator MinRotator;
+
+	FRotator MaxRotator;
+
+	FRotator ActualRotator;
+
+	bool IsActualRotatorMax;
 
 	UFUNCTION()
 	virtual void Tick(float DeltaSeconds) override;
@@ -19,6 +48,11 @@ class GLBPAINTBALL_API APBReactiveObjective : public APBObjective
 	virtual void BeginPlay() override;
 
 protected:
+
+	/** Character hit the painPlataform*/
+	UPROPERTY(VisibleAnywhere, Category = Damage)
+	class APBCharacter * ActiveCharacter;
+
 	/** Initial position of the objective's movement */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement)
 	ATargetPoint* FromPoint;
@@ -38,6 +72,15 @@ protected:
 	/** Maximum distance from the monitor for the objective to react */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Movement)
 	float MaxDist;
+
+
+	/** called when finish the overlap */
+	UFUNCTION()
+	void OnEndOverlap(AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	/** called up on collision */
+	UFUNCTION()
+	void OnOverlap(class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 
 private:
 	AActor * PlayerCharacter;
